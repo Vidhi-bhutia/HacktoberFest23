09d120e3690598daa63d1a22ac6dcbff35570b42
iamabhish23 <iamabhishek.tiwari23@gmail.com>
2023-10-01 17:16:24 +0530

    commit

--- java/LinearSearch.java
+++ java/LinearSearch.java
@@ -1,29 +1,22 @@
-public class LinearSearch {
-        public static void main(String args[]){
-                int[] nums = {24,45,23,12,34,53,11};
-                int target = 24;
-                int ans = linearSearch(nums,target);
-                System.out.println(ans);
+import java.util.*;
+public class linearSearch {
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the size: ");
+        int n=sc.nextInt();
+
+        int arr[]=new int[n];
+        System.out.print("Enter the elements: ");
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
         }
+        System.out.print("Enter the element : ");
+        int x= sc.nextInt();
 
-        static int linearSearch(int[] arr, int target){
-                if(arr.length == 0) {
-                        return  -1;
-                }
-
-//                for(int i =0;i<arr.length;i++){
-//                        int element = arr[i];
-//                        if(element == target){
-//                                return i;
-//                        }
-//                }
-
-                for (int element : arr){
-                        if(element == target){
-                                return element;
-                        }
-                }
-
-                return  -1;
+        for(int i=0; i<n; i++){
+            if(arr[i]==x){
+                System.out.print("x found at: "+i);
+            }
         }
+    }
 }
